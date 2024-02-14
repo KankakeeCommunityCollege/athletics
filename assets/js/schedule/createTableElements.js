559d8952ce80acc819e37ee888e72c383a1675b3
wdzajicek <tig.welder4@gmail.com>
2024-02-14 16:59:16 -0600

    Fix date sorting, add print button to game schedule pages...
    * Fix date sorting for first column of schedules:
      * Add hidden column containing date in ISO
        format (yyyy-mm-dd).
      * Sort formatted date column using hidden ISO
        date columns data.
    * Add print button/functionality to game schedules:
      * Create hidden date column for print layout.
      * Hide unecessary columns and add disclaimer
        paragraph to bottom of print layout.

--- assets/js/schedule/createTableElements.js
+++ assets/js/schedule/createTableElements.js
@@ -45,11 +45,11 @@ function createHeadingRow(thead, data) {
   return tr;
 }
 
-function createCells(tr, val, isSecondCell, location) {
+function createCells(tr, val, isThirdCell, location) {
   const td = document.createElement('td');
-  const className = location == 'Home' ? 'tables--red' : 'tables--blue';
+  const className = (location.search('Home') !== -1) ? 'tables--red' : 'tables--blue';
 
-  if (isSecondCell) td.classList.add(className);
+  if (isThirdCell) td.classList.add(className);
 
   td.innerHTML = val;
   tr.append(td);
@@ -59,7 +59,7 @@ function createCells(tr, val, isSecondCell, location) {
 let [win, loss, tie] = [0, 0, 0];
 
 function addRecordCountToData(data) {
-  let status = data[7].trim(); // trim() to ensure consistent data
+  let status = data[8].trim(); // trim() to ensure consistent data
   const statusIsPending = (status === '' || status.search(/^([Cc]ancelled|[Pp]ostponed|C)$/) !== -1);
   // Double-header games have the status of both games in a single cell - separated by a coma or semi-colon
   const statusArray = status.split(/,(?:\s+)?|;(?:\s+)?/);
@@ -81,18 +81,22 @@ function addRecordCountToData(data) {
   let tieCount = (tie === 0) ? '' : ` - ${tie}`;
   let record = (statusIsPending) ? '' : `${win} - ${loss}${tieCount}`;
 
-  data[10] = record; // Add the calculated record to the end of each row
+  data[11] = record; // Add the calculated record to the end of each row
   return data;
 }
 
 function createBodyRow(tbody, data) {
   const tr = document.createElement('tr');
-  const location = data[6];
+  const location = data[7];
 
   data = addRecordCountToData(data);
   tbody.append(tr);
-  data.forEach((cell, i) => createCells(tr, cell, i === 1, location))
+  data.forEach((cell, i) => createCells(tr, cell, i === 2, location))
   return tr;
+}
+
+function formatPrintDate(start, end) {
+  return (end === '' || end === ' ') ? start : `${start} - ${end}`;
 }
 
 function formatISODate(dateStr) {
@@ -116,8 +120,13 @@ function createTableBodyRows(tbody, data) {
     let [start, end, ...theRest] = row;
     const startDate = start !== '' ? formatDate(start) : ' ';
     const endDate = end !== '' ? formatDate(end) : '';
-    const isoDate = formatISODate(start);
-    const formattedRow = [isoDate, `${startDate}${endDate === '' ? '' : ' - ' + endDate}`, end, ...theRest];
+    const formattedRow = [
+      formatISODate(start),
+      formatPrintDate(start, end),
+      `${startDate}${endDate === '' ? '' : ' - ' + endDate}`,
+      end,
+      ...theRest
+    ];
 
     createBodyRow(tbody, formattedRow);
   });
@@ -133,7 +142,7 @@ function createTableElements(response) {
   const tableData = sheetData.slice(1, sheetData.length); // is an array of arrays
 
   const recordHeader = 'Record <span class="typography__muted-small-caps">(W - L - T)</span>'; // Add the final "Record" column which is calculated from Wins/Losses/Ties
-  const headerRow = ['iso_date', ...headerData, recordHeader];
+  const headerRow = ['iso_date', 'Date', ...headerData, recordHeader];
 
   createHeadingRow(thead, headerRow);
   createTableBodyRows(tbody, tableData);
--- assets/js/schedule/scheduleSheetsAPI.js
+++ assets/js/schedule/scheduleSheetsAPI.js
@@ -16,13 +16,35 @@ function start() {
   }).then(response => {
     createTableElements(response);
   }).then(() => {
+    const printIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 8h-1V3H6v5H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zM8 5h8v3H8V5zm8 12v2H8v-4h8v2zm2-2v-2H6v2H4v-4c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v4h-2z"/><circle cx="18" cy="11.5" r="1"/></svg>';
+
     $('#responsiveTable').DataTable({
       responsive: true, // Activate responsive powers GO!
       paging: false, // Don't paginate. Schedule should all be on one page
       'order': [], // Do not order = 'order': []
       'columnDefs': [
-        { 'visible': false, 'targets': [0, 2, 9] }, // Hide the 1st, 3rd, & 10th columns
-        { 'orderData': 0, 'targets': 1 } // Make the 2nd column use the 1st columns data for sorting purposes
+        { 'visible': false, 'targets': [0, 1, 3, 10] }, // Hide the 1st, 2nd, 4th, & 11th columns
+        { 'orderData': 0, 'targets': 2 } // Make the 3rd column use the 1st column's data for sorting purposes
+      ],
+      dom: 'Bfrti', // dom Option is needed to show (print) buttons - see: <https://datatables.net/reference/button/> and <https://datatables.net/reference/option/dom>
+      buttons: [ // Buttons extension to add print-button
+        {
+          extend: 'print', // Adds a print button
+          text: `Print&nbsp;${printIcon}`, // Modify print buttons text
+          // autoPrint: false, // Prevent print dialog from opening - useful for refining print layout in dev
+          exportOptions: {
+            // Columns to display in the print view (0-based index)
+            columns: [1, 4, 5, 6, 7/*, 8, 9, 11*/]
+          },
+          customize: function (win) { // Function to customize the print view - win = Window JS global
+            const table = win.document.body.querySelector('table');
+            const p = `<p style="text-align: center;">
+  <em>Game schedules are subject to change, check the athletics website for postponements and cancellations.</em>
+</p>`;
+
+            table.insertAdjacentHTML('afterend', p);
+          }
+        }
       ]
     });
   }).then(() => {
