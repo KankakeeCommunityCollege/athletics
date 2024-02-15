888350118090ce9485993c39603fe3524ff282d5
wdzajicek <tig.welder4@gmail.com>
2024-02-15 11:00:25 -0600

    Upgrade Nodejs to `v20.11.1`, upgrade FB SDK...
    * Upgrade Node.js to `v20.11.1`.
    * Upgrade Facebook SDK script.
    * Add preload link to head for FB SDK.

---
+++ assets/js/dist/244.30f092a2cdca84196d66.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkcc_athletics=self.webpackChunkkcc_athletics||[]).push([[244],{244:function(e,n,t){t.r(n);const c=document.querySelector(".js-more-btn");n.default=function(){c.addEventListener("click",(e=>{const n=e.target;"More ..."==n.innerHTML?n.innerHTML="Close":n.innerHTML="More ..."}))}}}]);
